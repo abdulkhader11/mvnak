9ca6321b0e0318fc82411334984b2fea646f5f68
Abdul Khader <Abdul Khader@Abdul>
2018-09-11 15:47:35 +0530

    multiple test cases commit

---
+++ com.selenium.seleak/src/test/java/com/selenium/seleak/First.java
@@ -0,0 +1,17 @@
+package com.selenium.seleak;
+
+import org.testng.annotations.Test;
+
+public class First {
+
+	
+	@Test
+	public  void ak () throws InterruptedException {
+		
+		System.out.println("This is first case");
+		
+		
+	}
+	}
+
+
